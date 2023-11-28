b3ef6b380ff513dc4a5250dcd40137175553866a
doiy <38238615+doiy@users.noreply.github.com>
2023-11-28 13:48:56 +0900

    test

--- yayoi-homepage/src/App.js
+++ yayoi-homepage/src/App.js
@@ -1,25 +1,28 @@
 import logo from './2.jpg';
 import './App.css';
+import React from 'react';
 import Header from './components/Header/Header';
 import DoctorIntroduction from './components/DoctorIntroduction/DoctorIntroduction';
 import PatientRegistration from './components/PatientRegistration/PatientRegistration';
 import ApplicationForm from './components/ApplicationForm/ApplicationForm';
 import SecondOpinion from './components/SecondOpinion/SecondOpinion';
-
 import TopPage from './components/TopPgae/TopPage';
+
 import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route path="/" element={<TopPage />} />
-        <Route path="/doctor_introduction" element={<DoctorIntroduction />} />
-        <Route path="/test" element={<PatientRegistration />} />
-        <Route path="/test2" element={<ApplicationForm />} />
-        <Route path="/second_opinion" element={<SecondOpinion />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<TopPage />} />
+          <Route path="/doctor_introduction" element={<DoctorIntroduction />} />
+          <Route path="/test" element={<PatientRegistration />} />
+          <Route path="/test2" element={<ApplicationForm />} />
+          <Route path="/second_opinion" element={<SecondOpinion />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
